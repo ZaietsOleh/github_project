f941d93b6c439afa3b20cfeeb3027852f984572d
Rodion <rodion.gramushniak@gmail.com>
2021-02-22 21:18:45 +0200

    add log in user fragment

--- app/src/main/java/com/githubuiviewer/ui/NavigationActivity.kt
+++ app/src/main/java/com/githubuiviewer/ui/NavigationActivity.kt
@@ -3,10 +3,16 @@ package com.githubuiviewer.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.githubuiviewer.R
+import com.githubuiviewer.sharedPrefsTools.SharedPref
 
 class NavigationActivity : AppCompatActivity(R.layout.activity_navigation) {
+
     val navigator by lazy {
         Navigator(supportFragmentManager, R.id.basic_fragment_holder)
+    }
+
+    private val sharedPreferences by lazy {
+        SharedPref(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +20,13 @@ class NavigationActivity : AppCompatActivity(R.layout.activity_navigation) {
         setContentView(R.layout.activity_navigation)
 
         navigator.showUserScreen()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //val response = githubUtils.getAccesToken(code)
+//        val token = "${response.tokenType} ${response.accessToken}"
+//        sharedPreferences.token = token
     }
 }
--- app/src/main/java/com/githubuiviewer/ui/loginScreen/LoginFragment.kt
+++ app/src/main/java/com/githubuiviewer/ui/loginScreen/LoginFragment.kt
@@ -1,9 +1,46 @@
 package com.githubuiviewer.ui.loginScreen
 
-import com.githubuiviewer.R
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import com.githubuiviewer.*
+import com.githubuiviewer.databinding.LoginFragmentBinding
 import com.githubuiviewer.ui.BaseFragment
 
 class LoginFragment : BaseFragment(R.layout.login_fragment) {
+
+    private lateinit var binding : LoginFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = LoginFragmentBinding.bind(view)
+        setupListeners()
+    }
+
+    private fun setupListeners(){
+        binding.btnSignIn.setOnClickListener {
+            startGitHubLogin()
+        }
+    }
+
+    private fun startGitHubLogin() {
+        val authIntent = Intent(Intent.ACTION_VIEW, buildAuthGitHubUrl())
+        startActivity(authIntent)
+    }
+
+    private fun buildAuthGitHubUrl(): Uri {
+        return Uri.Builder()
+            .scheme(schema)
+            .authority(com.githubuiviewer.host)
+            .appendEncodedPath("login/oauth/authorize")
+            .appendQueryParameter("client_id", clientId)
+            .appendQueryParameter("scope", scopes)
+            .appendQueryParameter("redirect_url", redirectUrl)
+            .build()
+    }
+
     companion object {
         fun newInstance() = LoginFragment()
     }
