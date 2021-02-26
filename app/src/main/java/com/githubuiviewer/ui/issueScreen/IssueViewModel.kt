b25ead69c7f43a5f4ccd1e2940a6b480850a11cf
Oleh Zaiets <59374316+ZGoblin@users.noreply.github.com>
2021-02-26 02:35:42 +0200

    move comment to repository

--- app/src/main/java/com/githubuiviewer/ui/issueScreen/IssueViewModel.kt
+++ app/src/main/java/com/githubuiviewer/ui/issueScreen/IssueViewModel.kt
@@ -6,12 +6,12 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.githubuiviewer.data.repository.GitHubRepository
 import com.githubuiviewer.datasource.api.DataLoadingException
 import com.githubuiviewer.datasource.api.GitHubService
 import com.githubuiviewer.datasource.api.NetworkException
 import com.githubuiviewer.datasource.api.UnauthorizedException
 import com.githubuiviewer.datasource.model.IssueCommentRepos
-import com.githubuiviewer.datasource.model.IssueDetailRepos
 import com.githubuiviewer.datasource.model.ReactionContent
 import com.githubuiviewer.tools.Emoji
 import com.githubuiviewer.tools.PER_PAGE
@@ -26,7 +26,8 @@ import java.io.IOException
 import javax.inject.Inject
 
 class IssueViewModel @Inject constructor(
-    private val gitHubService: GitHubService
+    private val gitHubService: GitHubService,
+    private val gitHubRepository: GitHubRepository
 ) : BaseViewModel() {
 
     val baseScope = baseViewModelScope
@@ -70,44 +71,9 @@ class IssueViewModel @Inject constructor(
     private suspend fun commentsFlow(): Flow<PagingData<IssueCommentRepos>> {
         return Pager(PagingConfig(PER_PAGE)) {
             PagingDataSource(baseViewModelScope) { currentPage ->
-                val result =
-                    gitHubService.getIssueComments(
-                        owner = issuesDetailsParameter.owner,
-                        repo = issuesDetailsParameter.repo,
-                        issue_number = issuesDetailsParameter.issue_number,
-                        per_page = PER_PAGE,
-                        page = currentPage
-                    )
-                if (currentPage == 0) {
-                    return@PagingDataSource addAuthorContent(result)
-                }
-                return@PagingDataSource result
+                gitHubRepository.getIssueComment(issuesDetailsParameter, currentPage)
             }
         }.flow.cachedIn(baseViewModelScope)
-    }
-
-    private suspend fun addAuthorContent(comments: List<IssueCommentRepos>): List<IssueCommentRepos> {
-        val author =
-            mapToIssueCommentRepos(
-                gitHubService.getIssueDetail(
-                    owner = issuesDetailsParameter.owner,
-                    repo = issuesDetailsParameter.repo,
-                    issue_number = issuesDetailsParameter.issue_number,
-                )
-            )
-        val mutable = comments.toMutableList()
-        mutable.add(0, author)
-        return mutable.toList()
-    }
-
-    private fun mapToIssueCommentRepos(issueDetail: IssueDetailRepos): IssueCommentRepos {
-        return IssueCommentRepos(
-            id = issueDetail.number,
-            user = issueDetail.user,
-            body = issueDetail.body,
-            created_at = issueDetail.created_at,
-            reactions = issueDetail.reactions
-        )
     }
 
     override fun unauthorizedException() {
